dea48ee476f795ffb786f9761b75c9b2b8a4ff4d
Rohit Goudar <rohitgoudar444@gmail.com>
2017-06-22 23:15:38 +0530

    response

---
+++ app/src/main/java/com/appdeveloperrohitgmail/middle_layer/ResponseCheck.java
@@ -0,0 +1,12 @@
+package com.appdeveloperrohitgmail.middle_layer;
+
+/**
+ * Created by Rohit Goudar on 22-06-2017.
+ */
+
+public class ResponseCheck {
+    String q_id,response;
+    public ResponseCheck() {
+
+    }
+}
